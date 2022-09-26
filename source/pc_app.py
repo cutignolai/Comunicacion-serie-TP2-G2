b56a5f9aca2e3c70923c7eb9a4ebc61364cc060a
Shalycht <pdalla@itba.edu.ar>
2022-09-26 14:56:27 -0300

    hay 3D, errores en texto, error en recibir data

--- source/pc_app.py
+++ source/pc_app.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from collections import deque
 
 import pygame
@@ -54,6 +55,21 @@ codes = {
     "5": 4
 }
 
+def InitPygame():
+    global display
+    pygame.init()
+    display = (1000, 600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    pygame.display.set_caption('TP2 Labo de micros G2')
+
+def InitGL():
+    #glClearColor((1.0/255*46),(1.0/255*45),(1.0/255*64),1)     #quita el  color del fondo
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+
+    gluPerspective(100, (display[0]/display[1]), 0.1, 50.0)    #estos dos desordenan todo
+    glTranslatef(0.0,0.0, -5)
 
 def drawText(position, textString, color):
     font = pygame.font.Font(None, 50)
@@ -61,6 +77,16 @@ def drawText(position, textString, color):
     textData = pygame.image.tostring(textSurface, "RGBA", True)
     glRasterPos3d(*position)
     glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (255,255,255), (0,0,0,255))  
+
+def NewDrawText(textString):     
+    font = pygame.font.SysFont ("Courier New",25, True)
+    textSurface = font.render(textString, True, (255,255,255), (0,0,0,255)) 
+    textData = pygame.image.tostring(textSurface, "RGBA", True)    
+    glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
+
 
 
 def smallDrawText(position, textString, color):
@@ -133,6 +159,7 @@ def ReadData():
                 incomming_info = coms.read()
                 print("USB received: ", incomming_info)
                 incomming_info = incomming_info.decode("ascii")
+                #incomming_info = coms.readline().decode('utf-8')
                 print("USB decoded: ", incomming_info)
 
                 if incomming_info == 'S':  # start sentinel
@@ -171,6 +198,114 @@ def ReadData():
             except:
                 print("Decoding error")
 
+def DrawData():
+    
+    drawText((-18, 12, -10), texts[0], (255, 255, 255))
+    smallDrawText((-28, 12.5, -18), "Roll: %d " % roll[0], (255, 255, 255))
+    smallDrawText((-28, 10, -18), "Pitch: %d " % pitch[0], (255, 255, 255))
+    smallDrawText((-28, 7.5, -18), "Yaw: %d " % yaw[0], (255, 255, 255))
+
+    drawText((-4, 12, -10), texts[1], (255, 255, 255))
+    smallDrawText((-7, 12.5, -18), "Roll: %d " % roll[1], (255, 255, 255))
+    smallDrawText((-7, 10, -18), "Pitch: %d " % pitch[1], (255, 255, 255))
+    smallDrawText((-7, 7.5, -18), "Yaw: %d " % yaw[1], (255, 255, 255))
+
+    drawText((12, 12, -10), texts[2], (255, 255, 255))
+    smallDrawText((18, 12.5, -18), "Roll: %d " % roll[2], (255, 255, 255))
+    smallDrawText((18, 10, -18), "Pitch: %d " % pitch[2], (255, 255, 255))
+    smallDrawText((18, 7.5, -18), "Yaw: %d " % yaw[2], (255, 255, 255))
+
+    drawText((-18, -2, -10), texts[3], (255, 255, 255))
+    smallDrawText((-28, -10, -18), "Roll: %d " % roll[3], (255, 255, 255))
+    smallDrawText((-28, -12.5, -18), "Pitch: %d " % pitch[3], (255, 255, 255))
+    smallDrawText((-28, -15, -18), "Yaw: %d " % yaw[3], (255, 255, 255))
+
+    drawText((-4, -2, -10), texts[4], (255, 255, 255))
+    smallDrawText((-7, -10, -18), "Roll: %d " % roll[4], (255, 255, 255))
+    smallDrawText((-7, -12.5, -18), "Pitch: %d " % pitch[4], (255, 255, 255))
+    smallDrawText((-7, -15, -18), "Yaw: %d " % yaw[4], (255, 255, 255))
+
+def axis():
+    glColor3f(0, 0, 1)
+
+    glBegin(GL_LINES)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, 2)
+    glEnd()
+
+    glColor3f(0, 1, 0)
+
+    glBegin(GL_LINES)
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 2, 0)
+    glEnd()
+
+    glColor3f(1, 0, 0)
+
+    glBegin(GL_LINES)
+    glVertex3f(0, 0, 0)
+    glVertex3f(2, 0, 0)
+    glEnd()
+
+    glColor3f(1, 1, 1)
+
+def DrawBoard():
+    
+    glBegin(GL_QUADS)
+    x = 0
+    
+    for surface in surfaces:
+        
+        for vertex in surface:  
+            glColor3fv((colors[x]))          
+            glVertex3fv(vertices[vertex])
+        x += 1
+    glEnd()
+
+def DrawGLL(index):
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    gluPerspective(90, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    glRotatef(pitch[index], 0, 0, 1)
+    glRotatef(roll[index], -1, 0, 0)
+    glRotatef(yaw[index], 0, 1, 0)
+
+    
+    #pygame.display.flip()
+
+def DrawGL(index):
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity() 
+    gluPerspective(90, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0, -5)   
+
+    glRotatef(pitch[index], 0, 0, 1)
+    glRotatef(roll[index], -1, 0, 0)
+    glRotatef(yaw[index], 0, 1, 0)
+
+    drawText((-4, 10, -10), texts[index], (255, 255, 255))
+    smallDrawText((-25, -15, -18), "Roll: %d " % roll[index], (255, 255, 255))
+    smallDrawText((-5, -15, -18), "Pitch: %d " % pitch[index], (255, 255, 255))
+    smallDrawText((15, -15, -18), "Yaw: %d " % yaw[index], (255, 255, 255))
+
+    #gnum = str(index)
+    #NewDrawText("Board "+ gnum)
+    #NewDrawText("Roll: {}° Pitch: {}° Yaw: {}°".format(round(roll[index],1),round(pitch[index],1), round(yaw[index],1)))
+    #DrawText("Roll: {}°               Pitch: {}°".format(round(myimu.Roll,1),round(myimu.Pitch,1)))
+    DrawBoard()
+    #pygame.display.flip()
+
+def EventManager(selected_board):
+    if selected_board == 0:
+        DrawData()
+    else:
+        DrawGL(selected_board-1)
+    
+    
 
 def main():
     global coms
@@ -180,12 +315,10 @@ def main():
     def on_closing():
         global run
         run = False
-        window.destroy()
 
-    pygame.init()
-    display = (800, 600)
-    pygame.display.set_caption('TP2 Labo de micros G2')
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    InitPygame()
+    InitGL()
+    DrawData()
 
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
 
@@ -207,18 +340,54 @@ def main():
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
+                #navigating through 3D boards
                 if event.key == pygame.K_LEFT:
-                    if selected_board > 0:
+                    if selected_board > 1:
                         selected_board -= 1
                 if event.key == pygame.K_LEFT:
-                    if selected_board == 0:
-                        selected_board = 4
+                    if selected_board == 1:
+                        selected_board = 5
                 if event.key == pygame.K_RIGHT:
                     if selected_board < 5:
-                        selected_board += 1
+                        if selected_board > 0:
+                            selected_board += 1
                 if event.key == pygame.K_RIGHT:
-                    if selected_board >= 5:
+                    if selected_board == 5:
+                        selected_board = 1
+                #on main stage it doesn't change
+                if event.key == pygame.K_LEFT:
+                    if selected_board == 0:
                         selected_board = 0
+                if event.key == pygame.K_RIGHT:
+                    if selected_board == 0:
+                        selected_board = 0
+                #selecting a display, 0 for general view, otherwise select a board from 1 to 5
+                if event.key == pygame.K_0:
+                    selected_board = 0
+                if event.key == pygame.K_1:
+                    selected_board = 1
+                if event.key == pygame.K_2:
+                    selected_board = 2
+                if event.key == pygame.K_3:
+                    selected_board = 3
+                if event.key == pygame.K_4:
+                    selected_board = 4
+                if event.key == pygame.K_5:
+                    selected_board = 5
+
+                #testing
+                if event.key == pygame.K_6:
+                    roll[1] += 30
+                    if roll[1] > 180:
+                        roll[1] -= 360
+                if event.key == pygame.K_7:
+                    pitch[1] -= 25
+                    if pitch[1] < -180:
+                        pitch[1] += 360
+                if event.key == pygame.K_8:
+                    yaw[1] += 10
+                    if yaw[1] > 180:
+                        yaw[1] -= 360
 
                 if event.type == pygame.KEYDOWN:
                     keys[event.key] = 1
@@ -231,12 +400,7 @@ def main():
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        drawText((positions[0] - 1, 2, -10), texts[selected_board], (255, 255, 255))
-        # smallDrawText((positions[0]-1, 1, -10), "(Press enter)",(255,255,255))
-
-        drawText((positions[0] - 2, -6, -18), "Roll: %d " % roll[selected_board], (255, 0, 0))
-        drawText((positions[0] - 2, -4, -18), "Pitch: %d " % pitch[selected_board], (0, 0, 255))
-        drawText((positions[0] - 2, -2, -18), "Yaw: %d " % yaw[selected_board], (0, 255, 0))
+        EventManager(selected_board)
 
         if enterPressed:
             showRoll[selected_board] = 0
@@ -248,7 +412,7 @@ def main():
             targetShowOrientacion[selected_board] = yaw[selected_board]
 
         pygame.display.flip()
-        # pygame.time.wait(10)
+        #pygame.time.wait(10)
 
 
 if __name__ == '__main__':
