1a7301594dc63803c68f6505ca2b7959a6354966
Shalycht <pdalla@itba.edu.ar>
2022-10-03 08:00:59 -0300

    imprime una matriz con cada valor. Sigo sin saber por qué no actualiza las funciones de dibujo

--- source/pc_app.py
+++ source/pc_app.py
@@ -15,10 +15,12 @@ import threading
 connected = False
 # buffer for communications via serial port
 coms = None
-
+global myflag
+myflag = True
 roll = np.zeros(7)
 pitch = np.zeros(7)
 yaw = np.zeros(7)
+keys = {pygame.K_RETURN: 0}
 
 texts = [
     "Board 0",
@@ -101,6 +103,8 @@ def manageInfo(info):
 def ReadData():
     global connected
     global coms
+    global myflag
+
 
     while True:
 
@@ -119,10 +123,12 @@ def ReadData():
                 print("Available ports: " + str(available_ports))
                 port = input("Select a port number: ")
 
-                coms = serial.Serial('COM3', baudrate=9600, timeout=1)
+                coms = serial.Serial('COM5', baudrate=9600, timeout=1)
 
                 print("Connected!")
+                myflag = True
                 connected = True
+
             except:
                 coms = None
                 print("Trying to connect ... ")
@@ -133,7 +139,7 @@ def ReadData():
                 # data that entries via serial port
                 port_data = ""
                 incomming_info = coms.read().decode("ascii")
-                print(incomming_info)
+                #print(incomming_info)
                 if incomming_info == 'S':  # start sentinel
 
                     while incomming_info != 'E':  # end sentinel
@@ -141,7 +147,7 @@ def ReadData():
                             # reads port data
                             incomming_info = coms.read()
                             incomming_info = incomming_info.decode("ascii")
-                            print("USB decoded: ", incomming_info)
+                            #print("USB decoded: ", incomming_info)
 
                             # adds data to the buffer for post processing
                             port_data += incomming_info
@@ -153,7 +159,10 @@ def ReadData():
                         try:
                             # process data
                             group, r, p, y = manageInfo(port_data)
-
+                            #print("Group: " , group)
+                            #print("r: " , r)
+                            #print("p: " , p)
+                            #print("y: " , y)
                             # updates the rpy value on their corresponding group
 
                             roll[group] = r
@@ -183,25 +192,30 @@ def DrawBoard():
 
 # each board is drew on difference positions, hence, they need different functions
 
-def DrawGL0():          
+def DrawGL0():
+    global myflag
 
-    glLoadIdentity() 
+    glLoadIdentity()
     # checks perspective
-    gluPerspective(90, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(90, (display[0] / display[1]), 0.1, 50.0)
     # translation matrix
-    glTranslatef(-5.5, 3, -5)               
+    glTranslatef(-5.5, 3, -5)
     # rotation matrix
-    glRotatef(pitch[0], -1, 0, 0)
-    glRotatef(roll[0], 0, 0, 1)
-    glRotatef(yaw[0], 0, -1, 0)
+
+
+
+    glRotatef(round(pitch[0], 1), -1, 0, 0)
+    glRotatef(round(roll[0], 1), 0, 0, 1)
+    glRotatef(round(yaw[0], 1), 0, -1, 0)
     # draw the 3D figure on the system that has been set
     DrawBoard()
+
 
 def DrawGL1():          
 
     glLoadIdentity()
     gluPerspective(90, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0, 3, -5)               
+    glTranslatef(0, 3, -5)
 
     glRotatef(pitch[1], -1, 0, 0)
     glRotatef(roll[1], 0, 0, 1)
@@ -261,71 +275,89 @@ def DrawGL5():
 # as in the previous functions, text must be written on different functions in order to be 
 # unaffected by the 3D translation matrix
 
-def BoardText0():          
+def BoardText0():
+    global myflag
 
     glLoadIdentity()
     gluPerspective(90, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(-5.5, 3, -5)  
+    glTranslatef(-5.5, 3, -5)
+    if (myflag):
+        # print("Group 0:")
+        # print("roll: ", roll[0])
+        # print("pitch:", pitch[0])
+        # print("yaw: ", yaw[0])
+        # hay chance de imprimirlo más lindo, pero no lo puedo probar. Sería
+        print("Group 0:        Group 1:         Group 2:")
+        print("roll: {}°       roll: {}°        roll: {}°".format(roll[0], roll[1], roll[2]))
+        print("pitch: {}°      pitch: {}°       pitch: {}°".format(pitch[0], pitch[1], pitch[2]))
+        print("yaw: {}°        yaw: {}°         yaw: {}°".format(yaw[0], yaw[1], yaw[2]))
 
     drawText((-18, 0, -10), texts[0], (255, 255, 255))
-    smallDrawText((-15, 3, -18), "Roll: %d " % roll[0], (255, 255, 255))
-    smallDrawText((-15, 1, -18), "Pitch: %d " % pitch[0], (255, 255, 255))
-    smallDrawText((-15, -1, -18), "Yaw: %d " % yaw[0], (255, 255, 255))
+    smallDrawText((-15, 3, -18), "Roll: " + str(roll[0]), (255, 255, 255))
+    smallDrawText((-15, 1, -18), "Pitch: " + str(pitch[0]), (255, 255, 255))
+    smallDrawText((-15, -1, -18), "Yaw: " + str(yaw[0]), (255, 255, 255))
 
 def BoardText1():           
-
+    
     glLoadIdentity()
     gluPerspective(90, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(-5.5, 3, -5)  
 
     drawText((0, 0, -10), texts[1], (255, 255, 255))
-    smallDrawText((10, 3, -18), "Roll: %d " % roll[1], (255, 255, 255))
-    smallDrawText((10, 1, -18), "Pitch: %d " % pitch[1], (255, 255, 255))
-    smallDrawText((10, -1, -18), "Yaw: %d " % yaw[1], (255, 255, 255))
+    smallDrawText((10, 3, -18), "Roll: " + str(roll[1]), (255, 255, 255))
+    smallDrawText((10, 1, -18), "Pitch: " + str(pitch[1]), (255, 255, 255))
+    smallDrawText((10, -1, -18), "Yaw: " + str(yaw[1]), (255, 255, 255))
 
-def BoardText2():           
+def BoardText2():  
 
     glLoadIdentity()
     gluPerspective(90, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(-5.5, 3, -5)  
+    glTranslatef(-5.5, 3, -5) 
 
     drawText((17, 0, -10), texts[2], (255, 255, 255))
-    smallDrawText((35, 3, -18), "Roll: %d " % roll[2], (255, 255, 255))
-    smallDrawText((35, 1, -18), "Pitch: %d " % pitch[2], (255, 255, 255))
-    smallDrawText((35, -1, -18), "Yaw: %d " % yaw[2], (255, 255, 255))
+    smallDrawText((35, 3, -18),"Roll: " + str(roll[2]), (255, 255, 255))
+    smallDrawText((35, 1, -18), "Pitch: " + str(pitch[2]), (255, 255, 255))
+    smallDrawText((35, -1, -18), "Yaw: " + str(yaw[2]), (255, 255, 255))
 
-def BoardText3():           
+def BoardText3(): 
+    global myflag          
 
     glLoadIdentity()
     gluPerspective(90, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(-5.5, 3, -5)  
+    glTranslatef(-5.5, 3, -5) 
+    if (myflag):
+        print("Group 3:        Group 4:         Group 5:")
+        print("roll: {}°       roll: {}°        roll: {}°".format(roll[3], roll[4], roll[5]))
+        print("pitch: {}°      pitch: {}°       pitch: {}°".format(pitch[3], pitch[4], pitch[5]))
+        print("yaw: {}°        yaw: {}°         yaw: {}°".format(yaw[3], yaw[4], yaw[5]))
+        print("")
 
     drawText((-18, -15, -10), texts[3], (255, 255, 255))
-    smallDrawText((-15, -20, -18), "Roll: %d " % roll[3], (255, 255, 255))
-    smallDrawText((-15, -22, -18), "Pitch: %d " % pitch[3], (255, 255, 255))
-    smallDrawText((-15, -24, -18), "Yaw: %d " % yaw[3], (255, 255, 255))
+    smallDrawText((-15, -20, -18), "Roll: " + str(roll[3]), (255, 255, 255))
+    smallDrawText((-15, -22, -18), "Pitch: " + str(pitch[3]), (255, 255, 255))
+    smallDrawText((-15, -24, -18), "Yaw: " + str(yaw[3]), (255, 255, 255))
 
-def BoardText4():           
+def BoardText4():         
 
     glLoadIdentity()
     gluPerspective(90, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(-5.5, 3, -5) 
 
     drawText((0, -15, -10), texts[4], (255, 255, 255))
-    smallDrawText((10, -20, -18), "Roll: %d " % roll[4], (255, 255, 255))
-    smallDrawText((10, -22, -18), "Pitch: %d " % pitch[4], (255, 255, 255))
-    smallDrawText((10, -24, -18), "Yaw: %d " % yaw[4], (255, 255, 255))
+    smallDrawText((10, -20, -18), "Roll: " + str(roll[4]), (255, 255, 255))
+    smallDrawText((10, -22, -18), "Pitch: " + str(pitch[4]), (255, 255, 255))
+    smallDrawText((10, -24, -18), "Yaw: " + str(yaw[4]), (255, 255, 255))
 
-def BoardText5():         
+def BoardText5():       
 
     glLoadIdentity()
     gluPerspective(90, (display[0] / display[1]), 0.1, 50.0)
     glTranslatef(-5.5, 3, -5) 
 
     drawText((17, -15, -10), texts[5], (255, 255, 255))
-    smallDrawText((35, -20, -18), "Roll: %d " % roll[5], (255, 255, 255))
-    smallDrawText((35, -22, -18), "Pitch: %d " % pitch[5], (255, 255, 255))
-    smallDrawText((35, -24, -18), "Yaw: %d " % yaw[5], (255, 255, 255))
+    smallDrawText((35, -20, -18), "Roll: " + str(roll[5]), (255, 255, 255))
+    smallDrawText((35, -22, -18), "Pitch: " + str(pitch[5]), (255, 255, 255))
+    smallDrawText((35, -24, -18), "Yaw: " + str(yaw[5]), (255, 255, 255))
 
 # draws each board with its respective 3D obj & text
 
@@ -351,37 +383,26 @@ def main():
     InitPygame()
     InitGL()
 
-    # to give a certain perspective until data is received
-    pitch[0] = -30
-    yaw[0] = -45
-    pitch[1] = -30
-    pitch[2] = -30
-    yaw[2] = 45
-    pitch[3] = 22
-    yaw[3] = -45
-    pitch[4] = 22
-    pitch[5] = 22
-    yaw[5] = 45
-
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
     glRotatef(0, 0, 0, 0)
 
     coms = None
-
-
     # starts reading the thread on the port
     
     t1 = threading.Thread(target=ReadData, args=())
     t1.start()
 
     # draws the menu and updates the display
+
     while True:
+
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         LoadBoards()
 
         pygame.display.flip()
+
 
 
 
